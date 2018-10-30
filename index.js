832970070da33ba3bbe86c55b01580fa779d352f
Cira Devlin <cira.devlin@leveluprpo.com>
2018-10-30 23:57:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const dodger = document.getElementById('dodger')
+
+console.log('i am woking')
+
+let dodgerPosition = 0 
+
+dodger.style.left = '380px'
+
+function moveDodger() {
+  
+}
+
+dodger.addEventListener('keydown', moveDodger)
+
